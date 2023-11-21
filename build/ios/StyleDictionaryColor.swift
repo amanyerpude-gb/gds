652ed5bf036793896fa39f801dc805e3bed404da
Aman Yerpude <aman@gimbooks.com>
2023-11-21 13:34:01 +0530

    added build folder

---
+++ build/ios/StyleDictionaryColor.swift
@@ -0,0 +1,19 @@
+
+//
+// StyleDictionaryColor.swift
+//
+
+// Do not edit directly
+// Generated on Tue, 21 Nov 2023 08:02:25 GMT
+
+
+import SwiftUI
+
+public class  {
+    public static let ColorBlue = [UIColor colorWithRed:0.000f green:0.000f blue:1.000f alpha:1.000f]
+    public static let ColorGreen = [UIColor colorWithRed:0.000f green:1.000f blue:0.000f alpha:1.000f]
+    public static let ColorRed = [UIColor colorWithRed:1.000f green:0.000f blue:0.000f alpha:1.000f]
+    public static let SizeLarge = 512.00f
+    public static let SizeMedium = 256.00f
+    public static let SizeSmall = 128.00f
+}
