c1497bc38b218bbf65580f008e4a8697760044c4
Aman Yerpude <aman@gimbooks.com>
2023-11-30 15:29:03 +0530

    IOS Transform result added

--- build.js
+++ build.js
@@ -1,8 +1,23 @@
 const StyleDictionary = require("style-dictionary");
-const { registerTransforms } = require("@tokens-studio/sd-transforms");
+const { registerTransforms, transforms } = require("@tokens-studio/sd-transforms");
 
 registerTransforms(StyleDictionary, {
     excludeParentKeys: true,
+});
+
+StyleDictionary.registerTransformGroup({
+    name: 'ios/tokens-studio',
+    transforms: [
+        ...transforms,
+        // transforms that are inside ios transformGroup usually
+        'attribute/cti',
+        'name/cti/pascal',
+        'color/UIColor',
+        'content/objC/literal',
+        'asset/objC/literal',
+        'size/remToPt',
+        'font/objC/literal'
+    ],
 });
 
 const sd = StyleDictionary.extend("config.json");
